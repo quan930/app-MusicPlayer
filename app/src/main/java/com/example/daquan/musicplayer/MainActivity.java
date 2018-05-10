927d87b2685eae44896c7869aa07e7c34a915277
quan <1583090876@qq.com>
2018-05-10 13:57:59 +0800

    full

--- app/src/main/java/com/example/daquan/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/daquan/musicplayer/MainActivity.java
@@ -1,128 +1,197 @@
 package com.example.daquan.musicplayer;
 
-import android.Manifest;
-import android.content.Context;
+
+import android.animation.ObjectAnimator;
+import android.annotation.SuppressLint;
+import android.content.ComponentName;
 import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.content.ServiceConnection;
 import android.content.res.Resources;
 import android.graphics.drawable.ColorDrawable;
 import android.media.MediaPlayer;
 import android.os.Build;
-import android.os.Environment;
 import android.os.Handler;
-import android.os.Message;
-import android.service.notification.StatusBarNotification;
-import android.support.v4.content.ContextCompat;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.PopupWindow;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
-import java.io.File;
-import java.io.IOException;
+
 import java.text.SimpleDateFormat;
-import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    private Button start;//开始停止键
-    private Button down;//下一首
-    private Button up;//上一首
-    private Button musicMenu;//歌单
+    private ImageButton start;//开始停止键
+    private ImageButton down;//下一首
+    private ImageButton up;//上一首
+    private ImageButton musicMenu;//歌单
+    private ImageButton single;//单曲循环
     private SeekBar seekBar;//进度条
-    private int sum;//歌曲总数
-    private int current = 0;//当前数
+    private ImageView coverImage;
     private TextView totalTime;//全部时间
     private TextView playingTime;//当前时间
     private TextView musicName;//歌曲名称
     private Handler handler = new Handler();
-    private MediaPlayer mediaPlayer;
-    private MusicPlayer musicPlayer;
-    private SimpleDateFormat time = new SimpleDateFormat("mm:ss");
     private PopupWindow popupWindow;//菜单浮窗
     private ListView listView;
+    private MusicService musicService;
+    private Boolean isPlay = false;//音乐开始键 停止为faluse
+    private PopupMenu popupMenu;
+    private static int musicState = 1;//歌曲状态，1为顺序，2为乱序，3为单曲
 
+    @SuppressLint("WrongConstant")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();//去掉标题栏
+        setContentView(R.layout.activity_main);
+        musicService = new MusicService();
+
+        Intent bind = new Intent(MainActivity.this,MusicService.class);
+        startService(bind);
+        bindServiceConnection();//绑定
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             //透明状态栏
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         }
-        setContentView(R.layout.activity_main);
-        start = (Button) findViewById(R.id.satrt);
-        down = (Button) findViewById(R.id.down);
-        up = (Button) findViewById(R.id.up);
-        musicMenu = (Button) findViewById(R.id.musicList);
+
+        coverImage = (ImageView) findViewById(R.id.imageView);
+        musicService.animator = ObjectAnimator.ofFloat(coverImage, "rotation", 0, 359);
+
+        start = findViewById(R.id.musicstart);
+        down = findViewById(R.id.musicbefore);
+        up =findViewById(R.id.musicup);
+        musicMenu = findViewById(R.id.musicList);
         totalTime = findViewById(R.id.alltime);
+        single = findViewById(R.id.single);
         playingTime = findViewById(R.id.currentTime);
         musicName = findViewById(R.id.musicName);
         seekBar = findViewById(R.id.Bar);
-        try {
-            musicPlayer = new MusicPlayer(Environment.getExternalStorageDirectory()+"/Music");
-            sum = musicPlayer.getSum();
-        } catch (IOException e) {
-            e.printStackTrace();
+        popupMenu = new PopupMenu(MainActivity.this,single);
+        popupMenu.getMenuInflater().inflate(R.menu.main, popupMenu.getMenu());
+
+        if(musicService.getMediaPlayer()==null){
+            musicService.initializePlayer();
+        }else {
+            if(musicService.getMediaPlayer().isPlaying()){
+//                start.setText("暂停");
+                start.setImageResource(R.drawable.stop);
+                musicService.AnimatorAction();
+                isPlay = false;
+                Log.d("音乐播放", "onCreate: ");
+            }else{
+                Log.d("没有播放", "onCreate: ");
+            }
         }
-        mediaPlayer = musicPlayer.getMediaPlayer();
+        if (musicState==1){//顺序播放
+            musicService.getMediaPlayer().setLooping(false);
+            single.setImageResource(R.drawable.circulation);
+        }else{
+            if (musicState == 2){//乱序播放
+                musicService.getMediaPlayer().setLooping(false);
+                single.setImageResource(R.drawable.music_shuffle_button);
+            }else {//单曲循环
+                musicService.getMediaPlayer().setLooping(true);//循环播放
+                single.setImageResource(R.drawable.lopping);
+            }
+        }
         musicMenu();//菜单浮窗
-        seekBar.setProgress(mediaPlayer.getCurrentPosition());
-        seekBar.setMax(mediaPlayer.getDuration());
-        totalTime.setText(time.format(mediaPlayer.getDuration()));//曲目时间
-        musicName.setText(musicPlayer.getMusicName());//曲目名称
 
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(start.getText().equals("开始")){
-                    start.setText("暂停");
-                    mediaPlayer.start();
+                if(isPlay){
+                    start.setImageResource(R.drawable.stop);
+                    isPlay = false;
+                    musicService.getMediaPlayer().start();
+                    musicService.AnimatorAction();
                 }else{
-                    mediaPlayer.pause();
-                    start.setText("开始");
+                    musicService.getMediaPlayer().pause();
+                    start.setImageResource(R.drawable.start);
+                    musicService.AnimatorAction();
+                    isPlay = true;
                 }
             }
         });
         //下一首
         down.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                mediaPlayer.reset();
-                mediaPlayer = musicPlayer.next();
-                seekBar.setProgress(mediaPlayer.getCurrentPosition());
-                seekBar.setMax(mediaPlayer.getDuration());
-                totalTime.setText(time.format(mediaPlayer.getDuration()));//曲目时间
-                musicName.setText(musicPlayer.getMusicName());//曲目名称
-                if(start.getText().equals("暂停")){
-                    mediaPlayer.start();
+            public void onClick(View v) {
+                musicService.getMediaPlayer().reset();
+                musicService.setCurrent(musicService.getCurrent()+1);
+                seekBar.setProgress(musicService.getMediaPlayer().getCurrentPosition());
+                seekBar.setMax(musicService.getMediaPlayer().getDuration());
+                totalTime.setText(new SimpleDateFormat("mm:ss").format(musicService.getMediaPlayer().getDuration()));//曲目时间
+                musicName.setText(musicService.getMusicName());
+                if(isPlay){
+                    musicService.getMediaPlayer().start();
                 }
             }
         });
         //上一首
         up.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                mediaPlayer.reset();
-                mediaPlayer = musicPlayer.before();
-                seekBar.setProgress(mediaPlayer.getCurrentPosition());
-                seekBar.setMax(mediaPlayer.getDuration());
-                totalTime.setText(time.format(mediaPlayer.getDuration()));//曲目时间
-                musicName.setText(musicPlayer.getMusicName());//曲目名称
-                if(start.getText().equals("暂停")){
-                    mediaPlayer.start();
+            public void onClick(View v) {
+                musicService.getMediaPlayer().reset();
+                musicService.setCurrent(musicService.getCurrent()-1);
+                seekBar.setProgress(musicService.getMediaPlayer().getCurrentPosition());
+                seekBar.setMax(musicService.getMediaPlayer().getDuration());
+                totalTime.setText(new SimpleDateFormat("mm:ss").format(musicService.getMediaPlayer().getDuration()));//曲目时间
+                musicName.setText(musicService.getMusicName());
+                if(isPlay){
+                    musicService.getMediaPlayer().start();
                 }
             }
         });
+        //单曲循环
+        single.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupMenu.show();
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()){
+                            case R.id.cir:
+                                popupWindow.dismiss();
+                                musicState = 1;
+                                musicService.getMediaPlayer().setLooping(false);
+                                single.setImageResource(R.drawable.circulation);
+                                break;
+                            case R.id.out:
+                                popupWindow.dismiss();
+                                musicState = 2;
+                                single.setImageResource(R.drawable.music_shuffle_button);
+                                musicService.getMediaPlayer().setLooping(false);
+                                break;
+                            case R.id.one:
+                                popupWindow.dismiss();
+                                musicState = 3;
+                                single.setImageResource(R.drawable.lopping);
+                                musicService.getMediaPlayer().setLooping(true);//循环播放
+                                break;
+                        }
+
+                        return true;
+                    }
+                });
+            }
+        });
+
         //歌单
         musicMenu.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -135,14 +204,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Log.d("current",String.valueOf(position));
-                mediaPlayer.reset();
-                musicPlayer.setCurrent(position);
-                seekBar.setProgress(mediaPlayer.getCurrentPosition());
-                seekBar.setMax(mediaPlayer.getDuration());
-                totalTime.setText(time.format(mediaPlayer.getDuration()));//曲目时间
-                musicName.setText(musicPlayer.getMusicName());//曲目名称
-                if(start.getText().equals("暂停")){
-                    mediaPlayer.start();
+                musicService.getMediaPlayer().reset();
+                musicService.setCurrent(position);
+                seekBar.setProgress(musicService.getMediaPlayer().getCurrentPosition());
+                seekBar.setMax(musicService.getMediaPlayer().getDuration());
+                totalTime.setText(new SimpleDateFormat("mm:ss").format(musicService.getMediaPlayer().getDuration()));//曲目时间
+                musicName.setText(musicService.getMusicName());
+                if(!isPlay){
+                    musicService.getMediaPlayer().start();
                 }
                 popupWindow.dismiss();
             }
@@ -150,21 +219,27 @@ public class MainActivity extends AppCompatActivity {
     }
     @Override
     protected void onResume() {
-        seekBar.setProgress(mediaPlayer.getCurrentPosition());
-        playingTime.setText(time.format(mediaPlayer.getCurrentPosition()));
+        seekBar.setProgress(musicService.getMediaPlayer().getCurrentPosition());
+        playingTime.setText(new SimpleDateFormat("mm:ss").format(musicService.getMediaPlayer().getCurrentPosition()));
         handler.post(runnable);
         super.onResume();
     }
     public Runnable runnable = new Runnable() {
+        @SuppressLint("WrongConstant")
         @Override
         public void run() {
-            seekBar.setProgress(mediaPlayer.getCurrentPosition());
-            playingTime.setText(time.format(mediaPlayer.getCurrentPosition()));
+            seekBar.setProgress(musicService.getMediaPlayer().getCurrentPosition());
+            seekBar.setMax(musicService.getMediaPlayer().getDuration());
+            totalTime.setText(new SimpleDateFormat("mm:ss").format(musicService.getMediaPlayer().getDuration()));//曲目时间
+//        musicName.setText(musicPlayer.getMusicName());//曲目名称
+            musicName.setText(musicService.getMusicName());
+            seekBar.setProgress(musicService.getMediaPlayer().getCurrentPosition());
+            playingTime.setText(new SimpleDateFormat("mm:ss").format(musicService.getMediaPlayer().getCurrentPosition()));
             seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
                 @Override
                 public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                     if (fromUser) {
-                        mediaPlayer.seekTo(seekBar.getProgress());
+                        musicService.getMediaPlayer().seekTo(seekBar.getProgress());
                     }
                 }
 
@@ -175,24 +250,35 @@ public class MainActivity extends AppCompatActivity {
 
                 @Override
                 public void onStopTrackingTouch(SeekBar seekBar) {
-//                    musicService.mediaPlayer.seekTo(seekBar.getProgress());
+
                 }
             });
-            if(start.getText().equals("暂停")&&!mediaPlayer.isPlaying()){
-                mediaPlayer.reset();
-                mediaPlayer = musicPlayer.next();
-                seekBar.setProgress(mediaPlayer.getCurrentPosition());
-                seekBar.setMax(mediaPlayer.getDuration());
-                totalTime.setText(time.format(mediaPlayer.getDuration()));//曲目时间
-                musicName.setText(musicPlayer.getMusicName());//曲目名称
-                mediaPlayer.start();
-            }
-            handler.postDelayed(runnable, 1000);
+            musicService.getMediaPlayer().setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    musicService.getMediaPlayer().reset();
+                    if(musicState==1){
+                        musicService.setCurrent(musicService.getCurrent()+1);
+                    }else{
+                        musicService.setCurrent(new Random().nextInt(musicService.getMun()));
+                    }
+                    seekBar.setProgress(musicService.getMediaPlayer().getCurrentPosition());
+                    seekBar.setMax(musicService.getMediaPlayer().getDuration());
+                    totalTime.setText(new SimpleDateFormat("mm:ss").format(musicService.getMediaPlayer().getDuration()));//曲目时间
+                    musicName.setText(musicService.getMusicName());
+                    musicService.getMediaPlayer().start();
+                }
+            });
+            handler.postDelayed(runnable, 100);
         }
     };
+
+
+    //菜单栏
     private void musicMenu(){
         listView = new ListView(this);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,musicPlayer.getMusicNameList());
+        musicService.getMusicNameList();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,musicService.getMusicNameList());
         listView.setAdapter(adapter);
         popupWindow = new PopupWindow(ViewGroup.LayoutParams.MATCH_PARENT,getResources().getDisplayMetrics().heightPixels/2);
         popupWindow.setOutsideTouchable(true);
@@ -201,7 +287,31 @@ public class MainActivity extends AppCompatActivity {
         ColorDrawable colorDrawable = new ColorDrawable();
         colorDrawable.setColor(res.getColor(R.color.background));
         popupWindow.setBackgroundDrawable(colorDrawable);
+        popupWindow.setAnimationStyle(R.style.Popupwindow);
     }
 
+    //绑定activity
+    private ServiceConnection sc = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            musicService = ((MusicService.MyBinder) iBinder).getService();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            musicService = null;
+        }
+    };
+    private void bindServiceConnection() {
+        Intent intent = new Intent(this, MusicService.class);
+        startService(intent);
+        bindService(intent, sc, this.BIND_AUTO_CREATE);
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("断开连接", "onDestroy: ");
+        unbindService(sc);
+    }
 }
 
---
+++ app/src/main/java/com/example/daquan/musicplayer/MusicService.java
@@ -0,0 +1,150 @@
+package com.example.daquan.musicplayer;
+
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.annotation.SuppressLint;
+import android.app.DownloadManager;
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Binder;
+import android.os.Environment;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.animation.LinearInterpolator;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MusicService extends Service {
+    public static ObjectAnimator animator;
+    public final IBinder binder = new MyBinder();
+    static private MediaPlayer mediaPlayer;
+    private int mun;//总音乐数
+    private int current = 0;//当前播放数
+    private List<File> musicList = new ArrayList<File>();
+    private List<String> nameList = new ArrayList<String>();
+    private String musicName;//当前播放的歌名
+
+    public class MyBinder extends Binder {
+        MusicService getService() {
+            return MusicService.this;
+        }
+    }
+    //当前播放数
+    public int getCurrent() {
+        return current;
+    }
+    //构造器
+    public MusicService(String string) {
+        list(new File(string));
+        mun=musicList.size();//返回总数
+    }
+    //构造器
+    public MusicService() {
+        list(new File(Environment.getExternalStorageDirectory()+"/Music"));
+        mun=musicList.size();//返回总数
+    }
+    //返回歌单
+    public List<String> getMusicNameList() {
+        return nameList;
+    }
+    //返回总数
+    public int getMun() {
+        return mun;
+    }
+    //换曲
+    public void setCurrent(int current) {
+        if(current>=musicList.size()){
+            current = 0;
+        }else{
+            if(current<0){
+                current = musicList.size()-1;
+            }
+        }
+        this.current = current;
+        try {
+            mediaPlayer.setDataSource((musicList.get(current)).getPath());
+            mediaPlayer.prepare();
+            musicName = musicList.get(current).getName().substring(0,musicList.get(current).getName().indexOf("."));//曲目名称
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    //获取歌曲名字
+    public String getMusicName() {
+        return musicName;
+    }
+    //MediaPlayer
+    public MediaPlayer getMediaPlayer() {
+        return mediaPlayer;
+    }
+    //初始播放
+    public void initializePlayer() {
+        try {
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDataSource((musicList.get(current)).getPath());//设置音频路径进入初始化状态
+            mediaPlayer.prepare();//进入就绪状态
+//            mediaPlayer.setLooping(true);//循环播放
+            musicName = musicList.get(current).getName().substring(0,musicList.get(current).getName().indexOf("."));//曲目名称
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return;
+    }
+    //音乐播放列表
+    private void list (File file){
+        if(file.isDirectory()){
+            //文件夹
+            File result [] = file.listFiles();
+            for(int i = 0;i < result.length;i++) {
+                if(result[i].isDirectory()){
+                    list(result[i]);
+                }else{
+//                    文件
+                    musicList.add(result[i]);
+                    nameList.add(result[i].getName().substring(0,result[i].getName().indexOf(".")));
+                }
+            }
+        }else{
+            //文件
+            musicList.add(file);
+            nameList.add(file.getName().substring(0,file.getName().indexOf(".")));
+        }
+    }
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+//        throw new UnsupportedOperationException("Not yet implemented");
+        return binder;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("启动", "onCreate: ");
+        try {
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDataSource((musicList.get(current)).getPath());//设置音频路径进入初始化状态
+            mediaPlayer.prepare();//进入就绪状态
+//            mediaPlayer.setLooping(true);//循环播放
+            musicName = musicList.get(current).getName().substring(0,musicList.get(current).getName().indexOf("."));//曲目名称
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @SuppressLint("WrongConstant")
+    public  void AnimatorAction() {
+        if (mediaPlayer.isPlaying()) {
+            animator.setDuration(5000);
+            animator.setInterpolator(new LinearInterpolator()); // 均速旋转
+            animator.setRepeatCount(ValueAnimator.INFINITE); // 无限循环
+            animator.setRepeatMode(ValueAnimator.INFINITE);
+            animator.start();
+        }else{
+            animator.end();
+        }
+    }
+}
